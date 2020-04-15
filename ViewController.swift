bf33c3e7e847ea1a0a39ca25cc2e09efd5d957d6
unc-dan <firstdan8989@gmail.com>
2020-04-15 19:15:48 +0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FollowHub
+//
+//  Created by Dan T on 15/04/2020.
+//  Copyright © 2020 Dan T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
