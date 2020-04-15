db2a54b94c57e49871634a745fc9df26fde4736a
unc-dan <firstdan8989@gmail.com>
2020-04-15 21:52:53 +0700

    Update FHButton.swift
    Created custom button class (FHButton)

---
+++ FHButton.swift
@@ -0,0 +1,38 @@
+//
+//  FHButton.swift
+//  FollowHub
+//
+//  Created by Dan T on 15/04/2020.
+//  Copyright © 2020 Dan T. All rights reserved.
+//
+
+import UIKit
+
+class FHButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    // This init is for handling storyboards, we have to put it here or xcode will complain
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
