8246529eeeae15a2a20b778e4f4be26c90dbcb34
unc-dan <firstdan8989@gmail.com>
2020-04-16 16:16:53 +0700

    Configured custom alert VC

---
+++ FHBodyLabel.swift
@@ -0,0 +1,38 @@
+//
+//  FHBodyLabel.swift
+//  FollowHub
+//
+//  Created by Dan T on 16/04/2020.
+//  Copyright © 2020 Dan T. All rights reserved.
+//
+
+import UIKit
+
+class FHBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor                   = .secondaryLabel
+        font                        = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth   = true
+        minimumScaleFactor          = 0.75
+        lineBreakMode               = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ FHTitleLabel.swift
@@ -0,0 +1,37 @@
+//
+//  FHTitleLabel.swift
+//  FollowHub
+//
+//  Created by Dan T on 16/04/2020.
+//  Copyright © 2020 Dan T. All rights reserved.
+//
+
+import UIKit
+
+class FHTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor                   = .label
+        adjustsFontSizeToFitWidth   = true
+        minimumScaleFactor          = 0.9
+        lineBreakMode               = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
