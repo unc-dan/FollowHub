54a367dddc4dcf6e95ee2614b8398a0923db312d
unc-dan <firstdan8989@gmail.com>
2020-04-18 12:06:59 +0700

    Configured custom flow layout
    3 column flow layout accounting for padding and item spacing

--- Screens/FollowerListVC.swift
+++ Screens/FollowerListVC.swift
@@ -35,10 +35,26 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width                        = view.bounds.width
+        let padding: CGFloat             = 12
+        let minimumItemSpacting: CGFloat = 10
+        let availableWidth               = width - (padding * 2) - (minimumItemSpacting * 2)
+        let itemWidth                    = availableWidth / 3
+        
+        let flowLayout                   = UICollectionViewFlowLayout()
+        flowLayout.sectionInset          = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize              = CGSize(width: itemWidth, height: itemWidth + 40) //+40 for a little more height to account for avatar label below avatar
+        
+        
+        return flowLayout
     }
     
     
